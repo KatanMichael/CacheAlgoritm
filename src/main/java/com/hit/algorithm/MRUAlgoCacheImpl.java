dd2e14ae8351336d7c8cfdad337e1bf31541c6dd
katanmichael <michaelkatan@gmail.com>
2018-03-19 18:53:06 +0200

    MRU Finished

--- src/main/java/com/hit/algorithm/MRUAlgoCacheImpl.java
+++ src/main/java/com/hit/algorithm/MRUAlgoCacheImpl.java
@@ -8,7 +8,6 @@ public class MRUAlgoCacheImpl <K,V> extends AbstractAlgoCache<K,V>
     private Hashtable<K,V> hashtable;
     private Stack<K> firstStack;
     private Stack<K> secondStack;
-    private int numberOfEnteris;
     private boolean removingFirstStack;
 
     public MRUAlgoCacheImpl(int capacity)
@@ -30,13 +29,64 @@ public class MRUAlgoCacheImpl <K,V> extends AbstractAlgoCache<K,V>
 
     public V putElement(K key, V value)
     {
+        V v;
+        v = null;
+        K k;
 
-        return null;
+        int totalStacksSize = firstStack.size() + secondStack.size();
+
+        if(totalStacksSize == capacity)
+        {
+            if(firstStack.size() == capacity)
+            {
+                secondStack.push(key);
+                hashtable.put(key,value);
+                removingFirstStack = true;
+                k = firstStack.pop();
+                v = hashtable.get(k);
+            }else
+            if(secondStack.size() == capacity)
+            {
+                firstStack.push(key);
+                hashtable.put(key,value);
+                removingFirstStack = false;
+                k = secondStack.pop();
+                v = hashtable.get(k);
+            }else
+            {
+                if(removingFirstStack)
+                {
+                    secondStack.push(key);
+                    hashtable.put(key,value);
+                    removingFirstStack = true;
+                    k = firstStack.pop();
+                    v = hashtable.get(k);
+                }else
+                {
+                    firstStack.push(key);
+                    hashtable.put(key,value);
+                    removingFirstStack = false;
+                    k = secondStack.pop();
+                    v = hashtable.get(k);
+
+                }
+            }
+
+
+
+        }else
+        {
+            hashtable.put(key,value);
+            firstStack.add(key);
+        }
+
+
+        return v;
     }
 
     public void removeElement(K key)
     {
-        numberOfEnteris--;
+
         if(!firstStack.removeElement(key))
         {
             secondStack.removeElement(key);
---
+++ src/main/test/com/hit/algorithm/MRUAlgoCacheImplTest.java
@@ -0,0 +1,57 @@
+package com.hit.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MRUAlgoCacheImplTest {
+
+    @Test
+    void getElement() {
+    }
+
+    @Test
+    void putElement()
+    {
+        MRUAlgoCacheImpl<Integer, Integer> mru = new MRUAlgoCacheImpl<>(3);
+        Integer myInt;
+
+        myInt = mru.putElement(1,1);
+        assertEquals(null,myInt);
+
+        myInt = mru.putElement(2,2);
+        assertEquals(null,myInt);
+
+        myInt = mru.putElement(3,3);
+        assertEquals(null,myInt);
+
+        myInt = mru.putElement(4,4);
+        assertEquals(3,myInt.hashCode());
+
+        myInt = mru.putElement(5,5);
+        assertEquals(2,myInt.hashCode());
+
+        myInt = mru.putElement(6,6);
+        assertEquals(1,myInt.hashCode());
+
+        myInt = mru.putElement(7,7);
+        assertEquals(6,myInt.hashCode());
+
+        myInt = mru.putElement(8,8);
+        assertEquals(5,myInt.hashCode());
+
+        myInt = mru.putElement(9,9);
+        assertEquals(4,myInt.hashCode());
+
+        myInt = mru.putElement(10,10);
+        assertEquals(9,myInt.hashCode());
+
+
+
+
+    }
+
+    @Test
+    void removeElement() {
+    }
+}
